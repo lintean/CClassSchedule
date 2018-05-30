3a6cc16c7d0cb4fad643f081c932e821376464f2
Lintean <904788247@qq.com>
2018-05-30 18:32:43 +0800

    version 1.1

--- main.cpp
+++ main.cpp
@@ -4,6 +4,13 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+
+    //应用qss样式表
+    QFile qss(":/new/prefix1/stylesheet.qss");
+    qss.open(QFile::ReadOnly);
+    a.setStyleSheet(qss.readAll());
+    qss.close();
+
     login w;
     w.show();
 
